e010af78fe6937e636980536a6703df2f4e7d417
Ayush Chaurasiya <2417ayushchaurasiya@gmail.com>
2026-08-21 14:16:22 +0530

    Second commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/user.routes');
 const dotenv = require('dotenv');
 const connectToDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const indexRouter = require('./routes/index.routes');
 const fileRouter = require('./routes/file.routes');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(cookieParser());
 
@@ -19,20 +21,35 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const dbConnection = connectToDB();
+
+app.use(async (req, res, next) => {
+    try {
+        await dbConnection;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
 app.use('/', indexRouter);
 
 app.use('/user', userRouter);
 
 app.use('/', fileRouter);
 
-const port = process.env.PORT || 3010;
+if (require.main === module) {
+    const port = process.env.PORT || 3010;
 
-connectToDB()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
+    dbConnection
+        .then(() => {
+            app.listen(port, () => {
+                console.log(`Server is running on port ${port}`);
+            });
+        })
+        .catch(() => {
+            process.exitCode = 1;
         });
-    })
-    .catch(() => {
-        process.exitCode = 1;
-    });
+}
+
+module.exports = app;
